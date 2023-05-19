edba5add2c650b29e1d55e9189db383c1ddaef65
izeriayounes <izeria.younes@ine.inpt.ma>
2023-05-19 16:26:23 +0100

    added 3rd project

--- src/components/Projects/Menu.jsx
+++ src/components/Projects/Menu.jsx
@@ -1,23 +1,29 @@
-import projet1 from '../../assets/projet1.PNG'
-import projet2 from '../../assets/projet2.PNG.jpg'
-
-
+import projet1 from '../../assets/projet1.PNG';
+import projet2 from '../../assets/projet2.jpg';
+import projet3 from '../../assets/projet3.jpg';
 
 const Menu = [
     {
       id: 1,
       image: projet1,
-      title: "Conserveur de notes",
-      category: "React Application",
+      title: "React Application",
+      category: "Conservateur de notes",
       url:'https://keeper-app-izeriayounes.vercel.app/',
     },
     {
       id: 2,
       image: projet2,
-      title: "Game build with JQuery",
-      category: "JQuery Application",
+      title: "A game that challenges the player's memory",
+      category: "Simon game",
       url:'https://simon-game-izeriayounes.vercel.app/',
     },
+    {
+      id: 3,
+      image: projet3,
+      title: "Application that grants security with authentification using mongoDB sessions",
+      category: "Secrets",
+      url:'https://secrets.herokuapp.com/',
+    }
 ]
 
 export default Menu
